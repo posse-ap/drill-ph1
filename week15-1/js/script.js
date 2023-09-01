401f15afc872772d1240ae6f8020847e56f61f85
kohei23n <kohei.s.n@gmail.com>
2023-09-01 14:44:58 +0900

    add: week15-1

---
+++ week15-1/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const articleElement = document.querySelector("#article");
+const toggleButtonElement = document.querySelector("#toggle-button");
+
+const toggleArticleVisibility = () => {
+  // 記事が表示されている場合は非表示に、非表示の場合は表示にする処理を書く
+};
+
+// ボタンをクリックしたら toggleArticleVisibility を実行する
